49f29227b981d4b1f67877b75a4b64abb0b14f98
8ito4 <101402102+8ito4@users.noreply.github.com>
2022-04-02 10:25:49 -0300

    Delete aula 64.py

--- aula 64.py
+++
@@ -1,8 +0,0 @@
-numero = 0
-contador = 0
-
-while numero != 999:
-    numero = int(input('digite um número: '))
-    contador +=1
-
-print ('voce digitou {} vezes e a soma entre eles fooi:{} '.format(numero, contador))
