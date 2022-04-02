84a6811d013fd5fa581892eac5a8cd3a48e2926a
8ito4 <101402102+8ito4@users.noreply.github.com>
2022-04-02 10:37:42 -0300

    Delete exercicio 66.py

--- exercicio 66.py
+++
@@ -1,9 +0,0 @@
-soma = cont = 0
-
-while True:
-    num = int(input('Digite um número: '))
-    if num == 999:
-        break
-    cont += 1
-    soma += num
-print ('a soma dos {} valores foi de {} '.format(cont,soma))
